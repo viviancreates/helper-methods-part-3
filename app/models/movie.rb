d1cc30c9932c035bef183107ea88c3de714fbb5f
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-10-11 17:39:02 +0000

    Added column for example only

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,7 @@
 #
 #  id          :bigint           not null, primary key
 #  description :text
+#  image_url   :string
 #  title       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
