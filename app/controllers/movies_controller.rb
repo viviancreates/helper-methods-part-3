c8a7882540bde834fc6ed96bbd51f595f88539cd
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-10-11 20:59:49 +0000

    Confused

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
   def new
     @movie = Movie.new
   end
@@ -14,11 +15,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params.fetch(:id))
-  end
-
-  def movie_params
-    params.require(:movie).permit(:title, :description)
+    
   end
 
   def create
@@ -35,12 +32,11 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
+
   end
 
   def update
-    @movie = Movie.find(params.fetch(:id))
-
+    
     
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie updated successfully."
@@ -50,10 +46,20 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params.fetch(:id))
+   
 
     @movie.destroy
 
     redirect_to movies_url, notice: "Movie deleted successfully."
+  end
+
+  private
+
+  def movie_params
+    movie_params = params.require(:movie).permit(:title, :description)
+  end
+
+  def set_movie
+    @movie = Movie.find(params.fetch(:id))
   end
 end
